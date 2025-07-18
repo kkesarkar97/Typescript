1b091c53cc03b61ef3b25d67e398d21a867aed84
kkesarkar97 <kkesarkar97@gmail.com>
2025-07-18 22:51:49 +0530

    Update something one

--- Example.js
+++ Example.js
@@ -2,3 +2,6 @@ var firstname = "Kaustubh";
 var lastname = "Kesarkar";
 var fullname = "Hello ! ".concat(firstname, " ").concat(lastname);
 console.log(fullname);
+var age = 25;
+var isEmployed = true;
+console.log("Age: ".concat(age, ", Employed: ").concat(isEmployed));
--- Example.ts
+++ Example.ts
@@ -2,3 +2,7 @@ let firstname: string = "Kaustubh";
 let lastname: string = "Kesarkar";
 let fullname: string = `Hello ! ${firstname} ${lastname}`;
 console.log(fullname);
+
+let age: number = 25;
+let isEmployed: boolean = true;
+console.log(`Age: ${age}, Employed: ${isEmployed}`);
